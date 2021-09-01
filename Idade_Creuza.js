f6553fc63e181b1cc5a0497d99b39239e7503a11
Wellington <wellington.multilaser@gmail.com>
2021-09-01 16:39:14 -0300

    Upando arquivo JS Idade

---
+++ Idade_Creuza.js
@@ -0,0 +1,7 @@
+let anoAtual = parseInt(prompt("Qual ano atual? "));
+let anoNasc = parseInt(prompt("Qual ano de nascimento? "));
+let idade = anoAtual - anoNasc;
+
+
+console.log("Sua idade é ", (idade) );
+
